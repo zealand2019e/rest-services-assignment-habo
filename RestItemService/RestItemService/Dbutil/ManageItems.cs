66679046e959dab47501010c6f336f12efb7db16
tomcsojn <44970244+tomcsojn@users.noreply.github.com>
2019-10-31 13:29:27 +0100

    Create ManageItems.cs

---
+++ RestItemService/RestItemService/Dbutil/ManageItems.cs
@@ -0,0 +1,30 @@
+﻿using ModelLib.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestItemService.Dbutil
+{
+    public class ManageItems
+    {
+        
+        
+
+        protected Item ReadNextElement(SqlDataReader reader)
+        {
+            Item item = new Item();
+            item.Id = reader.GetInt32(0);
+            item.Name = reader.GetString(1);
+            item.Quality = reader.GetString(2);
+            item.Quantity = reader.GetDouble(3);
+            return item;
+        }
+
+        public Item Get(int id) { }
+        public void Post(Item value) { }
+        public void Put(int id, Item value) { }
+        public void Delete(int id) { }
+    }
+}
